54a9a2bd01fd429836bb2d5e2840ed368aefc891
wsj <1021596238@qq.com>
2019-04-13 17:26:23 +0800

    test1

--- src/main/java/com/qf/controller/TestController.java
+++ src/main/java/com/qf/controller/TestController.java
@@ -3,6 +3,7 @@ package com.qf.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hahahah");
+        System.out.println("fdsfdsfds");
     }
     /*请在controller包后，添加子包，子包加上自己模块的名字*/
 }
