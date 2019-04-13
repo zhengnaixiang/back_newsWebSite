4fcb440380729e80f1af01ea05f03506698fac79
cczacks <2295980023@qq.com>
2019-04-13 16:04:52 +0800

    sout

--- src/main/java/com/qf/controller/TestController.java
+++ src/main/java/com/qf/controller/TestController.java
@@ -2,8 +2,7 @@ package com.qf.controller;
 
 public class TestController {
     public static void main(String[] args) {
-
+        System.out.println(1);
     }
     /*请在controller包后，添加子包，子包加上自己模块的名字*/
-    String a=null;
 }
