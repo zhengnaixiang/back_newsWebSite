06d49f3c9ecc87ff3efa6e7dce915f258d461101
MrAnt99 <xiedehui1995@Outlook.com>
2019-04-20 15:42:58 +0800

    后天粉丝模块的管理提交

---
+++ src/main/java/com/qf/fans/service/impl/FansServiceImpl.java
@@ -0,0 +1,30 @@
+package com.qf.fans.service.impl;
+
+import com.qf.Comment.mapper.CommentMapper;
+import com.qf.fans.dto.FansDto;
+import com.qf.fans.mapper.FansMapper;
+import com.qf.fans.service.FansService;
+import com.qf.fans.vo.FansVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FansServiceImpl implements FansService {
+
+  /*  @Autowired
+    FansMapper fansMapper;*/
+  private static ApplicationContext context=new ClassPathXmlApplicationContext("spring-mybatis.xml","spring-service.xml");
+    private static FansMapper fansMapper=context .getBean(FansMapper.class);
+
+    /**
+     * 获取粉丝分析列表的数据，并按照粉丝数降序输出
+      * @return
+     */
+    public List<FansDto> getAllFansData(FansVo fansVo) {
+        return fansMapper.getAllFansData(fansVo);
+    }
+}
