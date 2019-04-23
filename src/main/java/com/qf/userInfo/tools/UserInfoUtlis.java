787ef61a22fd5920457669dab47e0c1dde163887
cczacks <49503895+cczacks@users.noreply.github.com>
2019-04-23 19:48:18 +0800

    Update UserInfoUtlis.java

--- src/main/java/com/qf/userInfo/tools/UserInfoUtlis.java
+++ src/main/java/com/qf/userInfo/tools/UserInfoUtlis.java
@@ -7,15 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.UUID;
 
 public class UserInfoUtlis {
-    public static void setUser_img(CommonsMultipartFile file, UserInfo userInfo){
+    public static void setUser_img(CommonsMultipartFile file, UserInfo userInfo,HttpServletRequest request){
         String filename = file.getOriginalFilename();
         if(filename != null && !filename .equals("")){
             filename = UUID.randomUUID() + filename;
-            String path = UserInfo.class.getResource("/").getPath();
-            path = path.substring(0, path.indexOf("target")) + "src/main/webapp/images/" + filename;
+            String path = request.getSession().getServletContext().getRealPath("/images/head/")+File.separator+filename;
             File save = new File(path);
             if (!save.exists()) {
                 save.mkdirs();
@@ -25,8 +26,14 @@ public class UserInfoUtlis {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            String user_imageUrl = "images/" + filename;
-            userInfo.setUser_imageUrl(user_imageUrl);
+            InetAddress ia=null;
+            try {
+                ia = ia.getLocalHost();
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            String serverPath = String.format("%s://%s:%s%s%s", request.getScheme(), ia.getHostAddress(), request.getServerPort(), request.getContextPath(),"/images/head/")+filename;
+            userInfo.setUser_imageUrl(serverPath);
         }
     }
     public static void setObject(HttpServletRequest request,Object o){
